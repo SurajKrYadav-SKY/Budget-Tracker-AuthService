006f8dc9537fe3f2e6c46bae66b3fac63fa0203a
Suraj Kumar Yadav <suraj.mails99@gmail.com>
2025-04-02 21:41:37 +0530

    database connection done

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 const bodyParser = require("body-parser");
+const connect = require("./config/db");
 const express = require("express");
 const app = express();
-const dotenv = require("dotenv");
-dotenv.config();
+const { PORT } = require("./config/serverConfig");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-PORT = process.env.PORT;
-
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log("Server started at PORT:", PORT);
+  await connect();
+  console.log("MongoDB connected");
 });
